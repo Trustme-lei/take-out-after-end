dc06e30e04a613c8db1adc73e304a661c0956264
Trustme-lei <githubtrustmelei>
2020-04-22 17:38:09 +0800

    网关

--- cloud-gateway -outmod8736/src/main/java/com/js/ls/gateway/GatewayApplication.java
+++ cloud-gateway -outmod8736/src/main/java/com/js/ls/gateway/GatewayApplication.java
@@ -16,5 +16,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class GatewayApplication {
   public static void main(String[] args) {
     SpringApplication.run(GatewayApplication.class,args);
+
   }
 }
